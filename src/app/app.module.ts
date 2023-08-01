6635b9e15bc2db07ce072b6313b5c9e4d92c7426
tejas <tejasshah@gmail.com>
2023-08-01 19:56:50 +0530

    toaster navigate and httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -16,7 +20,10 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-signup',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class SignupComponent {
 
+  email = ""
+  firstName = ""
+  password = ""
+
+  constructor(private toastr: ToastrService,private router:Router,private httpClient:HttpClient) {}
+
+  signup(){
+    console.log(this.firstName+" "+this.email+" "+this.password);
+
+    //db save -> api 
+    this.toastr.success("Signup Done","",{timeOut:3000})
+    this.router.navigateByUrl("/login")
+    
+  }
 }
