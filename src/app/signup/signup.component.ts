9e4c66866ddeb9aac365cd6a92fb6336c034c9a6
tejas <tejasshah@gmail.com>
2023-08-02 20:04:34 +0530

    api call

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -20,6 +20,19 @@ export class SignupComponent {
     console.log(this.firstName+" "+this.email+" "+this.password);
 
     //db save -> api 
+   let data ={
+      "email":this.email ,
+      "firstName":this.firstName,
+      "password":this.password
+   }
+
+    this.httpClient.post("https://demopass.onrender.com/signup",data).subscribe(resp=>{
+      console.log(resp);
+      
+    },error=>{
+      console.log(error);
+      
+    })
     this.toastr.success("Signup Done","",{timeOut:3000})
     this.router.navigateByUrl("/login")
     
