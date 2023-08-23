5dca72aeb371295400f25f372056ef4a56062600
tejas <tejasshah@gmail.com>
2023-08-23 20:02:57 +0530

    error

--- src/app/reactive-signup/reactive-signup.component.ts
+++ src/app/reactive-signup/reactive-signup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+import { ValidatorFn } from '@angular/forms';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -14,14 +16,40 @@ export class ReactiveSignupComponent {
   constructor(){
     this.myForm  = new FormGroup({
       firstName: new FormControl("",Validators.required),
-      email:new FormControl("",Validators.required),
-      password:new FormControl("",Validators.required)
+      email:new FormControl("",[Validators.required,this.emailValid]),
+      password:new FormControl("",[Validators.required,Validators.minLength(8)])
     })
   }
 
   saveUser(){
     console.log(this.myForm.value);
     console.log(this.myForm.valid);
+    
+  }
+
+  emailValid(control:AbstractControl){ 
+    console.log("->"+control.hasError("required"));
+    if(control.hasError('required')){ 
+      console.log("ignore");
+      
+      return {
+        emailInvalid:false 
+      }
+    }else{
+      if(control.value.endsWith("@gmail.com") && control.value.length > 11) {
+        console.log("hit");
+        
+        return {
+          emailInvalid:false
+        }
+      }else{
+        console.log("success");
+        
+        return {
+          emailInvalid:true
+        }
+      }
+    }
     
     
   }
